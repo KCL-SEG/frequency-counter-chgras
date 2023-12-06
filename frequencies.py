7fb31716d0233fc04955736977b26d8850ab979a
chgras <chgraws@gmail.com>
2023-12-06 18:04:11 +0000

    finfish

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,11 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    for item in items:
+        currentItem = str(item)
+        if currentItem in frequencies:
+            frequencies[currentItem] = frequencies[currentItem] + 1
+        else:
+            frequencies[currentItem] = 1
+
     return frequencies
